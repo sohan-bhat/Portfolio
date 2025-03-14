863eeea4bc027463309b4be8e7473a071ae89949
sohan-bhat <sohanrambhat@gmail.com>
2025-03-13 23:04:14 -0500

    Added component files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Terminal from './Terminal';
+
+const Header = () => {
+  return (
+    <>
+        <Terminal />
+        <div>Header</div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Terminal.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/Terminal.css';
+  return (
+    <div>Terminal</div>
+  )
+}
+
+export default Terminal
