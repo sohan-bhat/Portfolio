07849f3f9de9edbe52089cc2ea042b6001740bbb
sohan-bhat <sohanrambhat@gmail.com>
2026-04-27 22:52:25 -0500

    Wire About, Robotics, and floating terminal into App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
+import About from './components/About';
 import Projects from './components/Projects';
+import Robotics from './components/Robotics';
 import Footer from './components/Footer';
+import FloatingTerminal from './components/FloatingTerminal';
 import './styles/App.css'
 
 function App() {
@@ -27,8 +30,11 @@ function App() {
     <Router>
         <div className='App'>
             <Header onAnimationComplete={handleAnimationComplete} />
+            <About />
             <Projects />
+            <Robotics />
             <Footer />
+            <FloatingTerminal isVisible={animationComplete} />
         </div>
     </Router>
   );
